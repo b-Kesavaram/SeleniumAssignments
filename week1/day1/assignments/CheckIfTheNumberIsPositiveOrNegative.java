d1c1a28e022c4950a00807af4af8b8513cd581c6
b-Kesavaram <114815559+b-Kesavaram@users.noreply.github.com>
2022-10-03 13:15:22 +0530

    Update CheckIfTheNumberIsPositiveOrNegative.java
    Completed

--- week1/day1/assignments/CheckIfTheNumberIsPositiveOrNegative.java
+++ week1/day1/assignments/CheckIfTheNumberIsPositiveOrNegative.java
@@ -16,5 +16,19 @@ public static void main(String[] args) {
 	 * 
 	 * "The number is neither positive nor negative"
 	 */
+	
+	int i=35;
+		if (i>0)
+		{
+			System.out.println("The number is positive");
+					   }
+		else
+					   {
+						   System.out.println("The number is negative");
+	}
+
+	}
+
 }
+
 }
