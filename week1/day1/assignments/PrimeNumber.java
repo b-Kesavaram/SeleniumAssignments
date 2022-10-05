1fe49ab58d570418e36cc53ba7d2a3148919a241
b-Kesavaram <114815559+b-Kesavaram@users.noreply.github.com>
2022-10-05 20:46:41 +0530

    Update PrimeNumber.java
    Completed

--- week1/day1/assignments/PrimeNumber.java
+++ week1/day1/assignments/PrimeNumber.java
@@ -77,6 +77,20 @@ public class PrimeNumber {
 			
 
 			// Print 'Not a Prime' when the condition doesn't match 
+		
+		int n =13;
+boolean flag = true;
+ for (int i=2;i<n;i++) 
+ {
+	 if(n%i==0)
+		 flag = false; 
+	 break;
+ }	 	
+	 System.out.println(flag);
+ 
+	}
+
+}
 
    }
 
